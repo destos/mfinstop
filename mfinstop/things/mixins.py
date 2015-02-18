a6d0b7bcfc209ed0b6cc12dedbb95cd6c9035300
Patrick Forringer <patrick@forringer.com>
2015-02-18 14:23:02 -0600

    fixing redirect issue when trying to report incidents.

--- mfinstop/things/mixins.py
+++ mfinstop/things/mixins.py
@@ -55,14 +55,12 @@ class UserMotiveFormKwargsMixin(object):
 class UserMotiveAccess(LoginRequiredMixin):
     def dispatch(self, request, *args, **kwargs):
         assert hasattr(self, 'get_motive')
-        if self.get_motive().user == request.user and not (
-                request.user.is_superuser):
-            if self.raise_exception:
-                raise PermissionDenied  # return a forbidden response
-            else:
-                return redirect_to_login(request.get_full_path(),
-                                         self.get_login_url(),
-                                         self.get_redirect_field_name())
+        if not request.user.is_authenticated():
+            return redirect_to_login(request.get_full_path(),
+                                     self.get_login_url(),
+                                     self.get_redirect_field_name())
+        if self.get_motive().user != request.user and not request.user.is_superuser:
+            raise PermissionDenied  # return a forbidden response
         response = super(UserMotiveAccess, self).dispatch(
             request, *args, **kwargs)
         return response
